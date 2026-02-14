f2ff3f24ee6a98f6e72a69f54105212c98221791
kev-69 <bismarkobuobi19@gmail.com>
2026-02-14 10:02:56 +0000

    feat: add About and Contact routes to the application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Plans from './pages/Plans';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
@@ -19,12 +21,13 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/plans" element={<Plans />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/subscribe/:planId" element={<Subscribe />} />
             <Route path="/payment/verify" element={<PaymentVerify />} />
-            {/* Add more routes as we build them */}
           </Routes>
         </main>
         <Footer />
