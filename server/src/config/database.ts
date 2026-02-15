08702d101e34926bc3d0431b3c7bee335e4f4b88
kev-69 <bismarkobuobi19@gmail.com>
2026-02-15 16:10:47 +0000

    fix: update PrismaClient logging configuration for better clarity in development

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -20,7 +20,9 @@ const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
   adapter,
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  log: process.env.NODE_ENV === 'development' 
+  ? ['error', 'warn'] 
+  : ['error'],
 });
 
 export default prisma;
